4f1da0fc65d96788d299d498e964b64048542ae4
Lê Việt Anh <56241260+code5thu2@users.noreply.github.com>
2020-06-29 09:53:24 +0700

    update project admin template

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+route::group(['prefix' => 'admin', 'namespace' => 'admin'], function () {
+    Route::get('', 'AdminController@index')->name('admin.index');
 });
